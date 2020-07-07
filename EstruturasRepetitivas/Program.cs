09598fc16792ddbfe19bcfb4b23ddd7ba0b77644
Leandro Sousa <leandroscjTI@gmail.com>
2020-07-07 00:47:38 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturasRepetitivas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EstruturasRepetitivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número");
+            int n = int.Parse(Console.ReadLine());
+
+            int cont_in = 0;
+            int cont_out = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20)
+                {
+                    cont_in = cont_in + 1;
+                }
+                else
+                {
+                    cont_out = cont_out + 1;
+                }
+            }
+            Console.Clear();
+            
+            Console.WriteLine(cont_in + " in");
+            Console.WriteLine(cont_out + " out");
+        }
+    }
+}
